651ede7d9e3658e79e958a33c582af8ad080a812
yurkenchik <yura.ilchshyn@gmail.com>
2024-10-27 02:44:54 +0300

    added method for saving user

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,7 +1,15 @@
-import {EntityRepository} from "@mikro-orm/postgresql";
+import {EntityManager, EntityRepository} from "@mikro-orm/postgresql";
 import {User} from "../entities/user.entity";
 import {Injectable} from "@nestjs/common";
 
 @Injectable()
 export class UserRepository extends EntityRepository<User> {
+    constructor(private readonly entityManager: EntityManager) {
+        super(entityManager, User);
+    }
+
+    async save(user: User): Promise<User> {
+        await this.entityManager.persistAndFlush(user);
+        return user;
+    }
 }
