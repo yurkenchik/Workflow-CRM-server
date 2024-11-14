118c84f9cd84c7a883ccc6976ee406e600df737e
yurkenchik <yura.ilchshyn@gmail.com>
2024-11-14 13:16:04 +0200

    updated password value object

--- src/common/value-objects/password.vo.ts
+++ src/common/value-objects/password.vo.ts
@@ -1,17 +1,17 @@
 import {ValueObject} from "../instances/value-object";
-import {InvalidEmailException} from "../exceptions/400-client/400/invalid-email.exception";
+import {InvalidPasswordException} from "../exceptions/400-client/400/invalid-password.exception";
 import * as bcrypt from "bcrypt";
 
 export class Password extends ValueObject<string>{
     constructor(public readonly password: string) {
         if (!Password.validatePassword(password)) {
-            throw new InvalidEmailException();
+            throw new InvalidPasswordException();
         }
         super(password);
     }
 
     private static validatePassword(password: string): boolean {
-        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
         return passwordRegex.test(password);
     }
 
