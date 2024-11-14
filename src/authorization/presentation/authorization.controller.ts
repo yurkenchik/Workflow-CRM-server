b9ab4a07f03f41bb3c209036cf6a482dd5ef15ea
yurkenchik <yura.ilchshyn@gmail.com>
2024-11-14 13:12:43 +0200

    added authorization controller

---
+++ src/authorization/presentation/authorization.controller.ts
@@ -0,0 +1,38 @@
+import {Body, Controller, Delete, Post} from "@nestjs/common";
+import {AuthorizationService} from "../infrastrcuture/services/authorization.service";
+import {CreateUserDto} from "../domain/dto/create-user.dto";
+import {User} from "../domain/entities/user.entity";
+import {LoginDto} from "../domain/dto/login.dto";
+import {AuthorizationResponseDto} from "../domain/dto/authorization-response.dto";
+import {ConfirmAuthorizationDto} from "../domain/dto/confirm-authorization.dto";
+import {UserId} from "../../common/guards/user-id.decorator";
+
+@Controller('auth')
+export class AuthorizationController {
+    constructor(private readonly authorizationService: AuthorizationService) {}
+
+    @Post("registration")
+    async registration(@Body() registrationDto: CreateUserDto): Promise<User> {
+        return this.authorizationService.registration(registrationDto);
+    }
+
+    @Post("login")
+    async login(@Body() registrationDto: LoginDto): Promise<User> {
+        return this.authorizationService.login(registrationDto);
+    }
+
+    @Post("confirm-registration")
+    async confirmRegistration(@Body() confirmAuthorizationDto: ConfirmAuthorizationDto): Promise<AuthorizationResponseDto> {
+        return this.authorizationService.confirmRegistration(confirmAuthorizationDto);
+    }
+
+    @Post("confirm-login")
+    async confirmLogin(@Body() confirmAuthorizationDto: ConfirmAuthorizationDto): Promise<AuthorizationResponseDto> {
+        return this.authorizationService.confirmLogin(confirmAuthorizationDto);
+    }
+
+    @Delete("logout")
+    async logout(@UserId()userId: string): Promise<void> {
+        return this.authorizationService.logout(userId);
+    }
+}
