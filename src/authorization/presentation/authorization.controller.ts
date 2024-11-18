1abb5fbd51661e62d13c24c765b430926de0cc9a
yurkenchik <yura.ilchshyn@gmail.com>
2024-11-19 01:46:50 +0200

    annotated authorization controller with swagger

--- src/authorization/presentation/authorization.controller.ts
+++ src/authorization/presentation/authorization.controller.ts
@@ -6,31 +6,47 @@ import {LoginDto} from "../domain/dto/login.dto";
 import {AuthorizationResponseDto} from "../domain/dto/authorization-response.dto";
 import {ConfirmAuthorizationDto} from "../domain/dto/confirm-authorization.dto";
 import {UserId} from "../../common/decorators/user-id.decorator";
+import {ApiBearerAuth, ApiBody, ApiHeader, ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 
+@ApiTags('Authorization')
 @Controller('auth')
 export class AuthorizationController {
     constructor(private readonly authorizationService: AuthorizationService) {}
 
+    @ApiOperation({ summary: 'Registration' })
+    @ApiBody({ type: CreateUserDto })
+    @ApiResponse({ status: 201, type: User })
     @Post("registration")
     async registration(@Body() registrationDto: CreateUserDto): Promise<User> {
         return this.authorizationService.registration(registrationDto);
     }
 
+    @ApiOperation({ summary: 'Login' })
+    @ApiBody({ type: LoginDto })
+    @ApiResponse({ status: 200, type: User })
     @Post("login")
     async login(@Body() registrationDto: LoginDto): Promise<User> {
         return this.authorizationService.login(registrationDto);
     }
 
+    @ApiOperation({ summary: 'Registration confirmation' })
+    @ApiBody({ type: ConfirmAuthorizationDto })
+    @ApiResponse({ status: 200, type: AuthorizationResponseDto })
     @Post("confirm-registration")
     async confirmRegistration(@Body() confirmAuthorizationDto: ConfirmAuthorizationDto): Promise<AuthorizationResponseDto> {
         return this.authorizationService.confirmRegistration(confirmAuthorizationDto);
     }
 
+    @ApiOperation({ summary: 'Login confirmation' })
+    @ApiBody({ type: ConfirmAuthorizationDto })
+    @ApiResponse({ status: 200, type: AuthorizationResponseDto })
     @Post("confirm-login")
     async confirmLogin(@Body() confirmAuthorizationDto: ConfirmAuthorizationDto): Promise<AuthorizationResponseDto> {
         return this.authorizationService.confirmLogin(confirmAuthorizationDto);
     }
 
+    @ApiOperation({ summary: 'Logging out of the account' })
+    @ApiBearerAuth()
     @Delete("logout")
     async logout(@UserId()userId: string): Promise<void> {
         return this.authorizationService.logout(userId);
