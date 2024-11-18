e4c266c5128e3a128924da0cf27af36dacc6bfce
yurkenchik <yura.ilchshyn@gmail.com>
2024-11-19 01:36:23 +0200

    added swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import {ConfigService} from "@nestjs/config";
 import {ValidationPipe} from "@nestjs/common";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {join} from "path";
 
 async function bootstrap(): Promise<void> {
     const configService = new ConfigService();
@@ -10,6 +12,15 @@ async function bootstrap(): Promise<void> {
 
     app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix("api");
+
+    const swaggerConfig = new DocumentBuilder()
+        .setTitle("Workflow CRM API documentation")
+        .setVersion("1.0.0")
+        .addTag("Backend API")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup("/api/docs", app, document);
 
     await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
