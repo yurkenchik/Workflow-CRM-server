eeae60a47a309fa163b2e486d78c19233de5a722
yurkenchik <yura.ilchshyn@gmail.com>
2024-12-06 01:38:43 +0200

    added command handlers

--- src/project/modules/providers/project.providers.ts
+++ src/project/modules/providers/project.providers.ts
@@ -1,11 +1,18 @@
 import { ProjectService } from "src/project/infrastructure/services/project.service";
 import { MemberService } from "src/project/infrastructure/services/member.service";
 import { GetProjectQueryHandler } from "src/project/infrastructure/queries/get-project/get-project.query.handler";
+import {
+    CreateProjectCommandHandler
+} from "src/project/infrastructure/commands/create-project/create-project.command.handler";
 
 export const projectServices = [
     ProjectService,
     MemberService,
 ];
+
+export const projectCommandHandlers = [
+    CreateProjectCommandHandler
+]
 
 export const projectQueryHandlers = [
     GetProjectQueryHandler
