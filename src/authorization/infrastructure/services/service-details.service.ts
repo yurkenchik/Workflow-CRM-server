39051aefe427a153b6ca65d0338711301098d352
yurkenchik <yura.ilchshyn@gmail.com>
2024-12-05 12:11:00 +0200

    updated services details service

--- src/authorization/infrastructure/services/service-details.service.ts
+++ src/authorization/infrastructure/services/service-details.service.ts
@@ -5,11 +5,14 @@ import { EntityManager, EntityRepository } from "@mikro-orm/postgresql";
 import { ServiceDetails } from "src/authorization/domain/entities/service-details.entity";
 import { AuthorizationUserService } from "./authorization-user.service";
 import { ServiceDetailsNotFoundException } from "src/common/exceptions/400-client/404/service-details-not-found.exception";
+import { AuthorizationAggregate } from "src/authorization/domain/aggregate/authorization.aggregate";
 
 import { CreateServiceDetailsDto } from "src/authorization/domain/dto/create-service-details.dto";
 
 @Injectable()
 export class ServiceDetailsService {
+    private readonly authorizationAggregate: AuthorizationAggregate = new AuthorizationAggregate();
+
     constructor(
         @InjectRepository(ServiceDetails)
         private readonly serviceDetailsRepository: EntityRepository<ServiceDetails>,
@@ -17,10 +20,18 @@ export class ServiceDetailsService {
         private readonly entityManager: EntityManager,
     ) {}
 
+    async getAllServiceDetails(): Promise<Array<ServiceDetails>> {
+        return this.serviceDetailsRepository
+            .createQueryBuilder()
+            .select(this.getServiceDetailsFields())
+            .getResultList();
+    }
+
     async getServiceDetailsById(serviceDetailsId: string): Promise<ServiceDetails> {
         const serviceDetails = await this.serviceDetailsRepository
             .createQueryBuilder()
             .where({ id: serviceDetailsId })
+            .select(this.getServiceDetailsFields())
             .getSingleResult();
 
         if (!serviceDetails) {
@@ -34,12 +45,11 @@ export class ServiceDetailsService {
         createServiceDetailsDto: CreateServiceDetailsDto
     ): Promise<ServiceDetails> {
         const user = await this.userService.getUserById(userId);
+        const serviceDetailsBuilder = this.authorizationAggregate.createServiceDetails(user, createServiceDetailsDto);
+
         const serviceDetailsInsertResult = await this.serviceDetailsRepository
             .createQueryBuilder()
-            .insert({
-                ...createServiceDetailsDto,
-                user
-            })
+            .insert(serviceDetailsBuilder)
             .returning("id")
             .execute();
 
@@ -49,5 +59,16 @@ export class ServiceDetailsService {
 
         await this.entityManager.persistAndFlush(serviceDetails);
         return serviceDetails;
+    }
+
+    private getServiceDetailsFields(): Array<keyof ServiceDetails> {
+        return [
+            "id",
+            "companyName",
+            "usagePurpose",
+            "teamPeopleRange",
+            "businessDirection",
+            "personBestDescriptor",
+        ];
     }
 }
