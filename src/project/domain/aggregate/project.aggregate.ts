c2ce3a3ef7b732babc406622322aac557c3ab259
yurkenchik <yura.ilchshyn@gmail.com>
2024-12-05 12:14:45 +0200

    updated project aggregate

--- src/project/domain/aggregate/project.aggregate.ts
+++ src/project/domain/aggregate/project.aggregate.ts
@@ -1,25 +1,78 @@
-import {Project} from "../entities/project.entity";
-import {Member} from "../entities/member.entity";
+import { Project } from "src/project/domain/entities/project.entity";
+import { Member } from "src/project/domain/entities/member.entity";
+import { User } from "src/authorization/domain/entities/user.entity";
+import { Task } from "src/project/domain/entities/task.entity";
+import { CreateProjectDto } from "src/project/domain/dto/create-project.dto";
+import { CreateTaskDto } from "src/project/domain/dto/create-task.dto";
+import { ProjectNotFoundException } from "src/common/exceptions/400-client/404/project-not-found.exception";
+import { MemberNotFoundException } from "src/common/exceptions/400-client/404/member-not-found.exception";
+import { UserNotFoundException } from "src/common/exceptions/400-client/404/user-not-found.exception";
 
 export class ProjectAggregate {
-    private readonly project: Project;
 
-    constructor(project: Project) {
-        this.project = project;
+    createProject(createProjectDto: CreateProjectDto): Project {
+        const { name, description  } = createProjectDto;
+
+        const project = new Project();
+        project.name = name;
+        project.description = description;
+
+        return project;
     }
 
-    getProject(): Project {
-        return this.project;
-    }
 
-    addMemberToProject(member: Member): Member {
-        const isMemberAdded = this.project.members.find((mb: Member) => mb.id === member.id);
+    createMember(user: User, project: Project): Member {
+        const member = new Member();
 
-        if (isMemberAdded) {
-            throw new Error("Member already exists");
+        if (!user) {
+            throw new UserNotFoundException();
         }
 
-        this.project.members.add(member);
+        if (!member) {
+            throw new MemberNotFoundException();
+        }
+
+        member.user = user;
+        member.project = project;
+
+        return member;
+    }
+
+    createTask(
+        createTaskDto: CreateTaskDto,
+        project: Project,
+        member: Member,
+    ): Task {
+        const { title, description, priority, status, dueDate } = createTaskDto;
+
+        const task = new Task();
+        task.title = title;
+        task.description = description;
+        task.priority = priority;
+        task.status = status;
+        task.dueDate = dueDate;
+
+        if (!project) {
+            throw new ProjectNotFoundException();
+        }
+
+        if (!member) {
+            throw new MemberNotFoundException();
+        }
+
+        task.project = project;
+
+        task.members.add(member);
+        member.tasks.add(task);
+
+        return task;
+    }
+
+    addMemberToProject(project: Project, member: Member): Member {
+        const existingMember = project.members.find((m: Member) => m.user.id === member.user.id);
+        member.project = project;
+        project.members.add(member);
+
         return member;
     }
 }
